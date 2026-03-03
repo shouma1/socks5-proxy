cfddeca6871cadfbbdefaa959fb6df5e09b4678e
shouma1 <102891047+shouma1@users.noreply.github.com>
2026-03-03 14:49:27 +0800

    Update config.go

--- config.go
+++ config.go
@@ -19,7 +19,7 @@ func ParseConfig() *Config {
 	cfg := &Config{}
 	flag.StringVar(&cfg.ListenAddr, "listen", "127.0.0.1:1080", "local SOCKS5 listen address")
 	flag.StringVar(&cfg.StatusAddr, "status", "127.0.0.1:8080", "HTTP status dashboard address")
-	flag.StringVar(&cfg.ScrapeURL, "url", "https://socks5-proxy.github.io/", "proxy list URL")
+	flag.StringVar(&cfg.ScrapeURL, "url", "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/all/data.txt/", "proxy list URL")
 	flag.DurationVar(&cfg.ScrapeInterval, "scrape-interval", 20*time.Minute, "scrape interval")
 	flag.DurationVar(&cfg.CheckTimeout, "check-timeout", 10*time.Second, "proxy check timeout")
 	flag.IntVar(&cfg.MaxConcurrent, "max-concurrent", 20, "max concurrent health checks")
